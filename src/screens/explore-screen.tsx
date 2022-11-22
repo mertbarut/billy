264aed96e157a64f6d3552193c75154bdd43ed2c
Mert Barut <mebarut@icloud.com>
2022-11-23 00:59:08 +0100

    made some dependency management; added skia to dependencies

--- src/screens/explore-screen.tsx
+++ src/screens/explore-screen.tsx
@@ -1,17 +1,43 @@
-import React from "react";
-import { Box, Text } from "native-base";
+import React, { useEffect } from "react";
+import { Box, Text, View } from "native-base";
+import {
+  Canvas,
+  Circle,
+  Group,
+  LinearGradient,
+  vec,
+} from "@shopify/react-native-skia";
+
+const width = 256;
+const height = 256;
+
+const PaintDemo = () => {
+  const r = width / 2;
+  return (
+    <Canvas style={{ width, height }}>
+      <Circle cx={r} cy={r} r={r}>
+        <LinearGradient
+          start={vec(0, 0)}
+          end={vec(2 * r, 2 * r)}
+          colors={["#00ff87", "#60efff"]}
+        />
+      </Circle>
+      <Group>
+        <LinearGradient
+          start={vec(2 * r, 2 * r)}
+          end={vec(4 * r, 4 * r)}
+          colors={["#0061ff", "#60efff"]}
+        />
+        <Circle cx={3 * r} cy={3 * r} r={r} />
+      </Group>
+    </Canvas>
+  );
+};
 
 export const ExploreScreen: React.FC<any> = () => {
   return (
-    <Box
-      w="100%"
-      h="100%"
-      bgColor="background.100"
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Text color="plainText.800">Explore Screen</Text>
-    </Box>
+    <View>
+      <PaintDemo />
+    </View>
   );
 };
